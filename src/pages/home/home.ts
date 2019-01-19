d780c98c80c25049552dbe18525e22f90a2ed1a5
Tjeu Hendriks <tjeu@rentman.nl>
2019-01-19 13:21:46 +0100

    add file chooser

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public fileChooser: FileChooser
+  ) {
 
   }
 
+  openPicker(): void {
+    this.fileChooser.open().then(value => {
+      console.log(value);
+    });
+  }
 }
